8d23c4ffc7e9ffc1fbca0ef5b79f708bda3b8821
Andres Pedraza <andrespedca@gmail.com>
2025-09-06 20:23:49 -0500

    draw player sprite screen

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
@@ -12,9 +14,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        print(dt)
 
 
 if __name__ == "__main__":
