dea79ceca9f34f51b89a1556b41ec2782b7b8782
Andres Pedraza <andrespedca@gmail.com>
2025-09-07 07:11:55 -0500

    added spawn asteroids

---
+++ asteroid.py
@@ -0,0 +1,13 @@
+import pygame
+from circleshape import *
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(surface=screen, center=self.position, radius=self.radius, width=2, color="white")
+
+    def update(self, dt):
+        self.position += self.velocity * dt
+        
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import *
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 
 def main():
     pygame.init()
@@ -8,21 +10,30 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
+    #asteroid = Asteroid(0, 39, 4)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidf = AsteroidField()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    for u in updatable:
+        print(u)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
-        dt = clock.tick(60) / 1000
+       
         updatable.update(dt)
         for sprite in drawable:
             sprite.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
